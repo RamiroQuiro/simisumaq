50b567ad52b94bc54e4b4aba1d4aa00fb1f7e707
ramiroquiro <rama.exe.13@gmail.com>
2025-11-22 11:16:54 -0300

    allowHost

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ export default defineConfig({
   server: {
     host: true,
     port: 4324,
-    allowedHosts: ["simisumaq.controlstock.online"],
+    allowedHosts: true,
   },
   devToolbar: {
     enabled: false,
