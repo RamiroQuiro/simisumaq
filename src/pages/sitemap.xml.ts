522caf0959b1c481092b73abc73c9cfdc46635b4
Ramiro Quiroga <ramiro.quiroga@cepsi.net.ar>
2026-03-09 11:08:01 -0300

    mejoras SEO y ampliacion a comision directiva

---
+++ src/pages/sitemap.xml.ts
@@ -0,0 +1,41 @@
+﻿import type { APIRoute } from "astro";
+
+const SITE = "https://asociacionsimisumaq.org";
+const pages = [
+  "/",
+  "/como-ayudar",
+  "/contacto",
+  "/equipo",
+  "/eventos",
+  "/galeria",
+  "/proyectos",
+  "/que-hacemos",
+  "/sobre-nosotros",
+  "/tienda",
+];
+
+export const GET: APIRoute = () => {
+  const lastmod = new Date().toISOString();
+
+  const urls = pages
+    .map(
+      (path) => `
+  <url>
+    <loc>${SITE}${path}</loc>
+    <lastmod>${lastmod}</lastmod>
+    <changefreq>weekly</changefreq>
+    <priority>${path === "/" ? "1.0" : "0.8"}</priority>
+  </url>`,
+    )
+    .join("");
+
+  const xml = `<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">${urls}
+</urlset>`;
+
+  return new Response(xml, {
+    headers: {
+      "Content-Type": "application/xml",
+    },
+  });
+};
